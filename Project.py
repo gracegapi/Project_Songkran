b5bf9fa8f4d2a948c2227b92915b65ddb02389cc
tangthairikoon <58070132@kmitl.ac.th>
2015-12-04 23:26:40 +0700

    update code

--- Project.py
+++ Project.py
@@ -6,15 +6,15 @@ def main():
     data = csv.reader(file)
     table = [row for row in data]
     print("แสดงจำนวนการเกิดอุบัติเหตุในช่วงเทศกาลสงกรานต์ระหว่างวันที่ 10-18")
-    year, day, age = False, False, False
+    year, days, age = False, False, False
     while year == False:
         year = to_range(input("ช่วงเวลา(Ex. 2554-2557) : "), list(range(2551, 2558)))
         if year == False:
             print("Please input year between 2551-2557.")
     province = input("จังหวัด(Ex. กรุงเทพมหานคร) : ")
-    while day == False:
-        day = to_range(input("วันที่เกิดเหตุ(Ex. 11-15) : "), list(range(10, 19)))
-        if day == False:
+    while days == False:
+        days = to_range(input("วันที่เกิดเหตุ(Ex. 11-15) : "), list(range(10, 19)))
+        if days == False:
             print("Please input day between 10-18.")
     sex = fix_sex([input("เพศ(ถ้าต้องการดูทั้งหมดพิมพ์ All) : ").lower()])
     while age == False:
@@ -22,9 +22,9 @@ def main():
         if age == False:
             print("Please input age between 0-200.")
     print("Processing...")
-    show, status, vehicle, parties_vehicle, drink_status, day_dict = {}, {}, {}, {}, {}, {}
+    total, status, vehicle, parties_vehicle, drink_status, day_dict = {}, {}, {}, {}, {}, {}
     for num in year: ## Create dict.
-        show[num] = 0
+        total[num] = 0
         status[num] = {"ผู้ขับขี่": 0, "ผู้โดยสาร": 0, "คนเดินเท้า": 0, "ไม่ทราบ": 0}
         vehicle[num] = {"รถเก๋ง/แท็กซี่": 0, "จักรยานยนต์": 0, "รถจักรยาน": 0, "ไม่ทราบ": 0, \
                         "สามล้อถีบ": 0, "สามล้อเครื่อง": 0, "ไม่มี/ล้มเอง": 0, "ปิคอัพ": 0, \
@@ -34,24 +34,24 @@ def main():
                                 "ไม่มี/ล้มเอง": 0, "ปิคอัพ": 0, "อื่นๆ": 0, "รถโดยสารใหญ่": 0, \
                                 "รถโดยสาร4ล้อ": 0, "รถบรรทุก": 0, "รถตู้": 0}
         drink_status[num] = {"ดื่ม": 0, "ไม่ดื่ม": 0, "ไม่ทราบ": 0}
-        day_dict[num] = {}
-        for days in day:
-            day_dict[num][days] = 0
+        day_dict[num] = {'10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, \
+                         '17': 0, '18': 0}
     for row in table:
         if row[0] in year:
-            if row[1] == province and row[2] in day and row[3] in sex and row[4] in age:
-                show[row[0]] += 1
+            if row[1] == province and row[2] in days and row[3] in sex and row[4] in age:
+                total[row[0]] += 1
                 status[row[0]][row[5]] += 1
                 vehicle[row[0]][row[6]] += 1
                 parties_vehicle[row[0]][row[7]] += 1
                 drink_status[row[0]][row[8]] += 1
                 day_dict[row[0]][row[2]] += 1
-    for num in sorted(show):
-        print("ผู้ประสบอุบัติเหตุในปี "+num+" : "+str(show[num])+" คน")
-        if show[num] != 0:
+    for num in sorted(total):
+        print("ผู้ประสบอุบัติเหตุในปี "+num+" : "+str(total[num])+" คน")
+        if total[num] != 0:
             print("* จำนวนผู้ประสบอุบัติเหตุในแต่ละวัน *")
             for day in sorted(day_dict[num]):
-                print("วันที่ "+day+" จำนวน "+str(day_dict[num][day])+" คน")
+                if day in days:
+                    print("วันที่ "+day+" จำนวน "+str(day_dict[num][day])+" คน")
             print("* สถานะของผู้ประสบอุบัติเหตุ *")
             for text in sorted(status[num]):
                 if status[num][text] != 0:
